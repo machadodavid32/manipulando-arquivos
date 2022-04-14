d1b550c53ec95a24fcb47afeb4ff610d81259a36
David Machado <machadodavid32@gmail.com>
2022-04-14 11:27:35 -0300

    Create escrever_em_arquivo.py

---
+++ escrever_em_arquivo.py
@@ -0,0 +1,9 @@
+with open('frutas.txt', 'w') as arquivo:
+    while True:
+        fruta = input('Informe uma fruta ou digite sair: ')
+        if fruta != 'sair':
+            arquivo.write(fruta + '\n')
+        else:
+            break
+            
+          
